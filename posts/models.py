d806097179bd26ddf23758b4abe7bed91d2f0a01
Yuuuuuuuuta <y.mitsu92@gmail.com>
2019-06-23 21:54:22 +0900

    add project

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    published = models.DateTimeField()
+    image = models.ImageField(upload_to = 'media/')
+    body = models.TextField()
+
+# Create your models here.
